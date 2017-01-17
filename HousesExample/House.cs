a6c9a2af867c816279cf2e440bfa2082f78601bd
JCarg <Jcarg108@gmail.com>
2017-01-17 14:28:15 -0500

    Added two methods for color

--- HousesExample/House.cs
+++ HousesExample/House.cs
@@ -8,5 +8,17 @@ namespace HousesExample
 {
     class House
     {
+        //For any defined variable this. can be used to call on that instance
+        //In the body fields are stored ex: Class house(rooms, colors)
+        //Variables can be assigned values, but would have all instances with same value
+        private string color;// Field
+        private int rooms;// Access Modifier public should not be used for fields
+        private int size;// Addding methods no longer need the keyword static
+        private int doors;
+
+        public void Paint(string color)
+        {
+            this.color = color;//.this goes back to the class to look for the variable and assigns it there.
+        }
     }
 }
