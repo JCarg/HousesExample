69d16466710360dd56dae8179d251a1a1536ec74
JCarg <Jcarg108@gmail.com>
2017-01-18 10:22:42 -0500

    Constructors added for displaying house info

--- HousesExample/House.cs
+++ HousesExample/House.cs
@@ -11,10 +11,35 @@ namespace HousesExample
         //For any defined variable this. can be used to call on that instance
         //In the body fields are stored ex: Class house(rooms, colors)
         //Variables can be assigned values, but would have all instances with same value
+        //Define fields up top. It does not have to be done within a program
         private string color;// Field
         private int rooms;// Access Modifier public should not be used for fields
         private int size;// Addding methods no longer need the keyword static
         private int doors;
+        static private int avgHousePrice;//static means there is one copy for each template
+
+        public House(string color, int rooms, int size, int doors)
+        {
+            this.color = color;
+            this.rooms = rooms;
+            this.size = size;
+            this.doors = doors;
+
+            
+            
+            // Constructors allow you to assign multiple values at once when setting up objects
+            // The variable must match. Ex: Person Variable=new Kid(); Does not work.
+            // Constructors are of the same species as objects
+            // Constructors never return a value. Does not need to have parameters if hard coding is used in the body.
+            // Constructors can share the same name, yet have different signatures.
+
+
+        }
+
+        public void DisplayHouseInfo()//When defining name for method must use Pascal Case.
+        {
+            Console.WriteLine("Color: " + color + "\nRooms: " + rooms + "\nSize: " + size + "\nDoors: " + doors);
+        }
 
         public void Paint(string color)
         {
--- HousesExample/Program.cs
+++ HousesExample/Program.cs
@@ -10,17 +10,21 @@ namespace HousesExample
     {
         static void Main(string[] args)
         {
-            House House1 = new House();
-            House1.Paint("Green");//Using methods from class
+            House House1 = new House("Blue",4,1000,5);
+            //House1.Paint("Green");//Using methods from class
             //Console.WriteLine(House1.CurrentColor());
-            House1.Paint("White");//Using methods from class
+            //House1.Paint("White");//Using methods from class
             //Console.WriteLine(House1.CurrentColor());
 
-            House House2 = new House();
-            House2.Paint("Yellow");
-            Console.WriteLine(House1.CurrentColor());
-            Console.WriteLine("This house has {0} paint", House1.CurrentColor());
-            Console.WriteLine("This house has {0} paint", House2.CurrentColor());
+            House House2 = new House("Mahogany",18,100,30);
+            //House2.Paint("Yellow");
+            //Console.WriteLine(House1.CurrentColor());
+            //Console.WriteLine("This house has {0} paint", House1.CurrentColor());
+            //Console.WriteLine("This house has {0} paint", House2.CurrentColor());
+
+            House1.DisplayHouseInfo();
+            House2.DisplayHouseInfo();
+
         }
     }
 }
